52d9baadb83b34aa8b0b28147eb6b556886116bd
Abhishekr2555 <148955093+Abhishekr2555@users.noreply.github.com>
2025-11-01 00:47:58 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,8 +18,8 @@ resource "aws_s3_bucket" "app_bucket" {
 }
 
 resource "aws_iam_policy" "app_policy" {
-  name        = "app-restricted-access"
-  description = "Policy used by instances with restricted permissions"
+  name        = "app-limited-access"  # Changed name to reflect the new limited access
+  description = "Policy used by instances with limited permissions"
 
   policy = jsonencode({
     Version = "2012-10-17"
