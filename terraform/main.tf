bd4d57c54f8f71aea459f39c8fc50900fe78a28f
Abhishekr2555 <148955093+Abhishekr2555@users.noreply.github.com>
2025-11-01 00:50:16 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,11 +14,11 @@ provider "aws" {
 
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "sample-app-terraform-bucket-12345"
-  acl    = "public-read"                        # Issue 1: public-read ACL
+  acl    = "private"  # Changed from "public-read" to "private" for better security
 }
 
 resource "aws_iam_policy" "app_policy" {
-  name        = "app-limited-access"
+  name        = "app-limited-access"  # Changed name to reflect the more limited access
   description = "Policy used by instances with limited permissions"
 
   policy = <<EOF
@@ -29,23 +29,34 @@ resource "aws_iam_policy" "app_policy" {
       "Effect": "Allow",
       "Action": [
         "s3:GetObject",
-        "s3:PutObject"
+        "s3:PutObject",
+        "s3:ListBucket"
       ],
-      "Resource": "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
+      "Resource": [
+        "arn:aws:s3:::sample-app-terraform-bucket-12345",
+        "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
+      ]
     }
   ]
 }
 EOF
 }
 
-resource "aws_security_group" "open_sg" {
-  name        = "open-sg"
-  description = "Security group with wide open access"
+resource "aws_security_group" "restricted_sg" {
+  name        = "restricted-sg"
+  description = "Security group with restricted access"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]                 # Issue 4: all ports open to the world
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]  # Restrict to internal network
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]  # Restrict to internal network
   }
 }
