ad46411b84908a9b6b9ca4eaa679c396742edf1d
Abhishekr2555 <148955093+Abhishekr2555@users.noreply.github.com>
2025-10-31 18:05:47 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,30 +14,32 @@ provider "aws" {
 
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "sample-app-terraform-bucket-12345"
-  acl    = "private"  # Fixed Issue 1: Changed from public-read to private
+  acl    = "private"  # Changed from "public-read" to "private" for better security
 }
 
 resource "aws_iam_policy" "app_policy" {
-  name        = "app-restricted-access"
-  description = "Policy used by instances with restricted permissions"
+  name        = "app-limited-access"  # Changed name to reflect the more limited access
+  description = "Policy used by instances with limited permissions"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.app_bucket.arn,
-          "${aws_s3_bucket.app_bucket.arn}/*"
-        ]
-      }
-    ]
-  })
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::sample-app-terraform-bucket-12345",
+        "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
+      ]
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_security_group" "restricted_sg" {
